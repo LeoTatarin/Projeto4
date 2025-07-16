62233b100b8124c4bc249ccb9e940235b5f87f11
LeoTatarin <tatarinleo@gmail.com>
2025-07-15 22:53:20 -0300

    adicionado FAQ a pagina

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+
+    const questions =document.querySelectorAll('[data-faq-question]');
     
     
-    for (let i = 0 ; i < buttons.length; i++) {
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
@@ -12,7 +14,19 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+
 })
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
